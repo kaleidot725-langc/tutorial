d80ee42841c7474b60833b8c7910f81f855a90ce
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-11-14 21:56:35 +0900

    feat: add initialize member

---
+++ src/initialize_member.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct MyPair {
+    int x;
+    string y;
+
+    MyPair() : x(123), y("hello") {
+    }
+};
+
+int main() {
+    MyPair p;
+    cout << "p.x = " << p.x << endl;
+    cout << "p.y = " << p.y << endl; 
+}
